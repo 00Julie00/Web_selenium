c97b173f3468c60644744a65857eba71606b4979
Julia Rusanova <elmarenelcorazon@gmail.com>
2023-04-02 00:00:17 +0300

    changed version of selenium dependencie

--- src/test/java/ru/netology/WebSeleniumTest.java
+++ src/test/java/ru/netology/WebSeleniumTest.java
@@ -3,11 +3,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,7 +12,7 @@ public class WebSeleniumTest {
     private WebDriver driver;
 
     @BeforeAll
-    static void setUpAll() {
+    static void setDriver() {
         WebDriverManager.chromedriver().setup();
     }
 
@@ -26,25 +23,24 @@ public class WebSeleniumTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999");
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
-    void TearDown() {
+    void setDown() {
         driver.quit();
         driver = null;
     }
 
     //все данные валидные
     @Test
-    void shouldAcceptValidFields() throws InterruptedException {
-        Thread.sleep(5000);
-        driver.findElement(By.cssSelector("[name='name']")).sendKeys("Алексей Алешин");
-        driver.findElement(By.cssSelector("[name= 'phone']")).sendKeys("+79118973456");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.tagName("button")).click();
-        String actualMessage = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText().trim();
-        String expectedMessage = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        Assertions.assertEquals(expectedMessage, actualMessage.strip());
+    void shouldAcceptValidFields()  {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Алексей Алешин");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79216780021");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actual);
     }
 }
